850e4709340fa068c30c557661f16b19cd4ba248
TannyDamet <118344382+TannyDamet@users.noreply.github.com>
2023-06-19 16:41:46 +0200

    Data IMU

--- Matlab/Kalman.m
+++ Matlab/Kalman.m
@@ -1,6 +1,6 @@
 % Importation des données des IMUs
-donnees_IMU1 = readmatrix('IMU1_data.csv'); % Assurez-vous que le format du fichier est correct
-donnees_IMU2 = readmatrix('IMU2_data.csv'); % Assurez-vous que le format du fichier est correct
+donnees_IMU1 = readmatrix('Bras_Othmane/sagital_1.csv'); % Assurez-vous que le format du fichier est correct
+donnees_IMU2 = readmatrix('Bras_Othmane/sagital_2.csv'); % Assurez-vous que le format du fichier est correct
 
 % Extraction des angles d'Euler et des accélérations des IMUs
 angles_IMU1 = donnees_IMU1(:, 1:3); % Angles d'Euler IMU 1 (roulis, tangage, lacet)
@@ -9,14 +9,14 @@ acc_IMU1 = donnees_IMU1(:, 4:6); % Accélérations IMU 1 (x, y, z)
 angles_IMU2 = donnees_IMU2(:, 1:3); % Angles d'Euler IMU 2 (roulis, tangage, lacet)
 acc_IMU2 = donnees_IMU2(:, 4:6); % Accélérations IMU 2 (x, y, z)
 
-% Paramètres du filtre de Kalman unscented
+% Paramètres du filtre de Kalman étendu (EKF)
 Q = eye(6); % Matrice de covariance du bruit de processus
 R = eye(6); % Matrice de covariance du bruit de mesure
 x_initial = zeros(6, 1); % Estimation initiale de l'état [angle_roulis, angle_tangage, angle_lacet, acc_x, acc_y, acc_z]
 P_initial = eye(6); % Matrice de covariance initiale de l'état
 
-% Filtrage des données des IMUs avec le filtre de Kalman unscented
-kf = unscentedKalmanFilter(@transitionFcn, @measurementFcn, x_initial, P_initial, 'HasAdditiveMeasurementNoise', true);
+% Filtrage des données des IMUs avec le filtre de Kalman étendu (EKF)
+kf = trackingEKF(@transitionFcn, @measurementFcn, x_initial, P_initial);
 kf.ProcessNoise = Q;
 kf.MeasurementNoise = R;
 
@@ -61,14 +61,14 @@ plot(t, acc_IMU2(:, 1), 'b', t, acc_IMU2_filtre(:, 1), 'g');
 title('IMU 2 - Accélération x');
 legend('Données brutes', 'Données filtrées');
 
-% Fonction de transition pour le filtre de Kalman unscented
+% Fonction de transition pour le filtre de Kalman étendu (EKF)
 function x_pred = transitionFcn(x)
     % À adapter selon votre modèle dynamique
     % Exemple : prédiction de l'état sans modification
     x_pred = x;
 end
 
-% Fonction de mesure pour le filtre de Kalman unscented
+% Fonction de mesure pour le filtre de Kalman étendu (EKF)
 function y_pred = measurementFcn(x)
     % À adapter selon votre modèle de mesure
     % Exemple : retourne directement l'état
